326d64acd0b0e2c5473393a209db841130577455
Richard Nunez <Antebios1@gmail.com>
2018-01-22 17:53:27 -0600

    Add more functionality to extract Product Code and properly format for better uninstall. (#2)
    * Function to get information from MSI package fle.
    
    Made it more fail-proof.  I made sure that these cases work:
    1. $Product parameter will work with Product Name with spaces.  The BTDF creates the name as "$(ProjectName) for BizTalk $(ProductVersion)"
    2. $Product code will work with actual Guid, with and without braces.
    3. $Product will work with path to MSI OR wildcard path to MSI, "C:\SomePath\*.msi".
    
    As you can see I am using a special function in a NEW file that will parse information from an actual MSI package file.  I am extracting the Product Code and making sure it is in a valid GUID string format that msiexec can use.   Why am I doing this?  During the BTDF deployment I copy the original MSI to the install dir.  So, that later when I do another deployment next time I set the $Product parameter as a wildcard path to the install directory "C:\Program Files (x86)\ProjectName\*.msi" and thus it will use the original MSI to uninstall the original package.  I then have another step to delete the MSI after the uninstall (because it doesn't not uninstall what it didn't place there to begin with).

--- MSIUninstall/UnInstall-BTDFApplication.ps1
+++ MSIUninstall/UnInstall-BTDFApplication.ps1
@@ -6,24 +6,32 @@
     [string]$Arguments
 )
 . "$PSScriptRoot\Init-BTDFTasks.ps1"
+. "$PSScriptRoot\Get-MSIFileInformation.ps1"
 
 $InstallGuid = [Guid]::Empty
+
 if (-not [Guid]::TryParse($Product, [ref] $InstallGuid)) {
-    if (Test-Path -Path $Product -ErrorAction SilentlyContinue) {
-        $MSI = Get-Item -Path $Product -ErrorAction Stop
-        $Name = [Regex]::Match($MSI.BaseName,'^(\.?[a-zA-Z]+)*').Value
+    if (Test-Path -Path "$Product" -ErrorAction SilentlyContinue) {
+        $MSI = Get-Item -Path "$Product" -ErrorAction Stop
+        $FoundMSIGuid = Get-MSIFileInformation -Path "$MSI" -Property "ProductCode"
+        if (-not [Guid]::TryParse($FoundMSIGuid, [ref] $InstallGuid)) {
+            $Name = [Regex]::Match($MSI.BaseName,'^(\.?[a-zA-Z]+)*').Value
+        }
     } else {
-        $Name = $Product
+        $Name = "$Product"
     }
-    $InstallGuid = Get-ChildItem $UninstallPath | Where-Object { ( $_ | Get-ItemProperty -Name DisplayName -ErrorAction SilentlyContinue).DisplayName -eq $Name } | Select-Object -ExpandProperty PSChildName
-    if ($null -eq $InstallGuid) {
-        Write-Host ("##vso[task.logissue type=error;] Product not found [{0}]" -f $Name)
+
+    if([Guid]::Empty -eq $InstallGuid) {
+        $InstallGuid = Get-ChildItem $UninstallPath | Where-Object { ( $_ | Get-ItemProperty -Name DisplayName -ErrorAction SilentlyContinue).DisplayName -eq "$Name" } | Select-Object -ExpandProperty PSChildName
+        if ($null -eq $InstallGuid) {
+            Write-Host ("##vso[task.logissue type=error;] Product not found [{0}]" -f $Name)
+        }
     }
 }
 
 $msiexec = 'msiexec.exe'
 $args = [string[]]@(
-    "/x $InstallGuid"
+    "/x {0:B}"  -f $InstallGuid
     "/qn"
 )
 if (-not [string]::IsNullOrWhiteSpace($Arguments)) {
